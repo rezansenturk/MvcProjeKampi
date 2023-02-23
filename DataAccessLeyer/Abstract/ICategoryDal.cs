6a86d3da068af98c6a9b8a6bf41725436fd09294
Rezan <116518302+rezansenturk@users.noreply.github.com>
2023-02-24 00:35:51 +0300

    Add project files.

---
+++ DataAccessLeyer/Abstract/ICategoryDal.cs
@@ -0,0 +1,27 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface ICategoryDal
+    {
+        // CRUD 
+        // type Name (); Metotları tanımlarken tipi ismi ve parantez aç kapa noktalı virgül
+
+        List<Category> List();
+
+        void Insert(Category p);
+
+        void Update(Category p);
+
+        void Delete(Category p);
+
+        // CRUD Operasyonlarının gerçekleşeceği metotlar bu şekildedir 
+
+
+    }
+}
